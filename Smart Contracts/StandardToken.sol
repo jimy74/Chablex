b7f99c1fe5368cc9a611bd467f2b4c806fb58577
Jimmy Paris <jimmyparis74@gmail.com>
2017-09-27 01:47:38 +0200

    Update StandardToken.sol

--- Smart Contracts/StandardToken.sol
+++ Smart Contracts/StandardToken.sol
@@ -27,10 +27,7 @@ contract StandardToken is BasicToken {
    */
   function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
     var _allowance = allowed[_from][msg.sender];
-
-    // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
-    // require (_value <= _allowance);
-
+    require (_value <= _allowance);
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
     allowed[_from][msg.sender] = _allowance.sub(_value);
